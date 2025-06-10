2d4c19865890bcf364446db92ee5b105388b59f2
o.akim <o.akim@TC231>
2025-06-10 19:20:27 +0500

    Add Constants

---
+++ sourses/Imperium.Core/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Imperium.Core
+{
+    public static class Constants
+    {
+        public static readonly Guid SYSTEM_UESER_ID = new Guid("00000000-0000-0000-0000-000000000001");
+    }
+}
